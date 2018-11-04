dea892e099eb66ab7f0bbb26f5595f43873ffe5d
Jarek Kazmierczak <jarekk@jk-macpro.local>
2018-11-04 15:41:47 -0800

    Updates

--- DataScienceTrack/01-aml-walkthrough/04-deploy/score.py
+++ DataScienceTrack/01-aml-walkthrough/04-deploy/score.py
@@ -6,14 +6,14 @@ from sklearn.linear_model import LogisticRegression
 import numpy as np
 import random
 import tensorflow as tf
-from tensorflow.keras.applications import vgg16
+from tensorflow.keras.applications import resnet50
 
 from azureml.core.model import Model
 
 def init():
     # Instantiate VGG16 featurizer
     global featurizer
-    featurizer = vgg16.VGG16(
+    featurizer = resnet50.ResNet50(
             weights = 'imagenet', 
             input_shape=(224,224,3), 
             include_top = False,
@@ -22,7 +22,7 @@ def init():
     # Load the model
     global model
     # retreive the path to the model file using the model name
-    model_path = Model.get_model_path('aerial_sklearn.pkl')
+    model_path = Model.get_model_path(model_name = 'model.pkl')
     model = joblib.load(model_path)
   
 
@@ -30,7 +30,7 @@ def run(raw_data):
     # convert json to numpy array
     img = np.array(json.loads(raw_data)['data'])
     # normalize as required by ResNet50
-    img = vgg16.preprocess_input(img)
+    img = resnet50.preprocess_input(img)
     # extract bottleneck features
     features = featurizer.predict(img)
     # make prediction
